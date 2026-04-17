b8d7c11a2506fde9c8ed88acc958f0446f35061e
stnickcoin-cod <st.nickcoin@gmail.com>
2026-04-17 11:36:54 +0300

    add new branch with Lesson6.1

--- src/test/java/ArithmeticCalculatorTest.java
+++ src/test/java/ArithmeticCalculatorTest.java
@@ -1,30 +1,32 @@
 import org.example.ArithmeticCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArithmeticCalculatorTest {
+    @DisplayName("сложение")
     @Test
     void testAddition() {
         assertEquals(5, ArithmeticCalculator.add(2, 3));
     }
-
+    @DisplayName("вычетание")
     @Test
     void testSubtraction() {
         assertEquals(-1, ArithmeticCalculator.subtract(2, 3));
     }
-
+    @DisplayName("умножение")
     @Test
     void testMultiplication() {
         assertEquals(6, ArithmeticCalculator.multiply(2, 3));
     }
-
+    @DisplayName("деление")
     @Test
     void testDivision() {
         assertEquals(2.0, ArithmeticCalculator.divide(6, 3), 0.001);
     }
-
+    @DisplayName("деление на 0")
     @Test
     void testDivisionByZero() {
         assertThrows(IllegalArgumentException.class,
--- src/test/java/TriangleAreaCalculatorTest.java
+++ src/test/java/TriangleAreaCalculatorTest.java
@@ -1,21 +1,23 @@
 import org.example.TriangleAreaCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TriangleAreaCalculatorTest {
+    @DisplayName("площадь корректного треугольника")
     @Test
      void testTriangleArea() {
         assertEquals(6.0, TriangleAreaCalculator.calculateArea(3, 4, 5), 0.001);
     }
-
+    @DisplayName("площадь с отрицательной стороной")
     @Test
      void testNegativeSides() {
         assertThrows(IllegalArgumentException.class,
                 () -> TriangleAreaCalculator.calculateArea(-1, 2, 3));
     }
-
+    @DisplayName("сумма двух сторон меньше третьей")
     @Test
      void testInvalidTriangle() {
         assertThrows(IllegalArgumentException.class,
