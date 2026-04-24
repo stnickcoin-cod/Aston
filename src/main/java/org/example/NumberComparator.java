e3f614a2793c7597919320e99689fb84b65a8135
stnickcoin-cod <st.nickcoin@gmail.com>
2026-04-24 09:21:38 +0300

    Delete src/main/java/org/example/NumberComparator.java

--- src/main/java/org/example/NumberComparator.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class NumberComparator {
-    public static int compare(int a, int b) {
-        if (a > b) return 1;
-        if (a < b) return -1;
-        return 0;
-    }
-}
